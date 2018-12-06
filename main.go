d8ffdab12b27ac023d631f085ce38da97721e825
Benjamin Borbe <bborbe@rocketnews.de>
2018-12-06 14:43:14 +0100

    add author params and refactoring

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"flag"
+	"context"
+	flag "github.com/bborbe/flagenv"
 	"fmt"
 	"github.com/golang/glog"
 	"github.com/pkg/errors"
@@ -21,24 +22,29 @@ func main() {
 	glog.CopyStandardLogTo("info")
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	var message string
-	var versionString string
-	flag.StringVar(&message, "message", "", "Message used for commit and changelog")
-	flag.StringVar(&versionString, "version", "", "Version used for commit and changelog")
+	app := &App{}
+	flag.StringVar(&app.AuthorName, "git-author-name", "", "Author Name")
+	flag.StringVar(&app.AuthorEmail, "git-author-email", "", "Author Email")
+	flag.StringVar(&app.Message, "message", "", "Message used for commit and changelog")
+	flag.StringVar(&app.Version, "version", "", "Version used for commit and changelog")
+	flag.StringVar(&app.Repo, "repo", "", "Git Repo")
 
 	_ = flag.Set("logtostderr", "true")
 	flag.Parse()
 
-	version, err := ParseVersion(versionString)
-	if err != nil {
-		if glog.V(2) {
-			glog.Fatalf("%+v", err)
-		} else {
+	if app.Repo == "" {
+		var err error
+		app.Repo, err = os.Getwd()
+		if err != nil {
 			glog.Fatal(err)
 		}
 	}
 
-	if err := bumpVersion(message, *version); err != nil {
+	if err := app.Validate(); err != nil {
+		glog.Fatal(err)
+	}
+
+	if err := app.Run(context.Background()); err != nil {
 		if glog.V(2) {
 			glog.Fatalf("%+v", err)
 		} else {
@@ -47,6 +53,97 @@ func main() {
 	}
 
 	glog.V(0).Infof("done")
+}
+
+type App struct {
+	Message     string
+	Version     string
+	AuthorName  string
+	AuthorEmail string
+	Repo        string
+}
+
+func (a *App) Validate() error {
+	if a.Message == "" {
+		return errors.New("Message missing")
+	}
+	if a.Version == "" {
+		return errors.New("Version missing")
+	}
+	if a.AuthorName == "" {
+		return errors.New("AuthorName missing")
+	}
+	if a.AuthorEmail == "" {
+		return errors.New("AuthorEmail missing")
+	}
+	if a.Repo == "" {
+		return errors.New("Repo missing")
+	}
+	return nil
+}
+
+func (a *App) Run(ctx context.Context) error {
+	version, err := ParseVersion(a.Version)
+	if err != nil {
+		return err
+	}
+
+	r, err := git.PlainOpen(a.Repo)
+	if err != nil {
+		return errors.Wrap(err, "open git directory failed")
+	}
+
+	w, err := r.Worktree()
+	if err != nil {
+		return errors.Wrap(err, "get worktree failed")
+	}
+
+	_, err = r.Tag(version.String())
+	if err == nil {
+		return errors.New("tag already exists")
+	}
+	glog.V(2).Infof("tag not found")
+
+	changelog, err := ioutil.ReadFile(changelogFilename)
+	if err != nil {
+		changelog = []byte(defaultChangelog)
+	}
+
+	changelog = re.ReplaceAll(changelog, []byte(fmt.Sprintf("$1\n## %s\n\n- %s\n$2", version.String(), a.Message)))
+	err = ioutil.WriteFile(changelogFilename, changelog, 0600)
+	if err != nil {
+		return errors.Wrap(err, "write CHANGELOG.md failed")
+	}
+	glog.V(2).Infof("changelog updated")
+
+	_, err = w.Add(changelogFilename)
+	if err != nil {
+		return errors.Wrap(err, "add file failed")
+	}
+
+	signature := &object.Signature{
+		Name:  a.AuthorName,
+		Email: a.AuthorEmail,
+		When:  time.Now(),
+	}
+	commit, err := w.Commit(a.Message, &git.CommitOptions{
+		Author: signature,
+	})
+
+	obj, err := r.CommitObject(commit)
+	if err != nil {
+		return errors.Wrap(err, "commit failed")
+	}
+
+	_, err = r.CreateTag(version.String(), obj.Hash, &git.CreateTagOptions{
+		Tagger:  signature,
+		Message: version.String(),
+	})
+	if err != nil {
+		return errors.Wrap(err, "create tag failed")
+	}
+
+	return nil
 }
 
 func ParseVersion(versionNumber string) (*Version, error) {
@@ -83,79 +180,7 @@ func (v Version) String() string {
 
 var re = regexp.MustCompile(`(?s)^(.*?)(\n##\s.*)$`)
 
-func bumpVersion(message string, version Version) error {
-	pwd, err := os.Getwd()
-	if err != nil {
-		return errors.Wrap(err, "get working directory failed")
-	}
-	r, err := git.PlainOpen(pwd)
-	if err != nil {
-		return errors.Wrap(err, "open git directory failed")
-	}
-
-	w, err := r.Worktree()
-	if err != nil {
-		return errors.Wrap(err, "get worktree failed")
-	}
-
-	status, err := w.Status()
-	if err != nil {
-		return errors.Wrap(err, "get status failed")
-	}
-
-	clean := status.IsClean()
-	if !clean {
-		return errors.New("git repo is not clean")
-	}
-
-	_, err = r.Tag(version.String())
-	if err == nil {
-		return errors.New("tag already exists")
-	}
-	glog.V(2).Infof("tag not found")
-
-	changelogFilename := "CHANGELOG.md"
-	changelog, err := ioutil.ReadFile(changelogFilename)
-	if err != nil {
-		changelog = []byte(defaultChangelog)
-	}
-
-	changelog = re.ReplaceAll(changelog, []byte(fmt.Sprintf("$1\n## %s\n\n- %s\n$2", version.String(), message)))
-	err = ioutil.WriteFile(changelogFilename, changelog, 0600)
-	if err != nil {
-		return errors.Wrap(err, "write CHANGELOG.md failed")
-	}
-	glog.V(2).Infof("changelog updated")
-
-	_, err = w.Add(changelogFilename)
-	if err != nil {
-		return errors.Wrap(err, "add file failed")
-	}
-
-	signature := &object.Signature{
-		Name:  "Benjamin Borbe",
-		Email: "bborbe@rocketnews.de",
-		When:  time.Now(),
-	}
-	commit, err := w.Commit(message, &git.CommitOptions{
-		Author: signature,
-	})
-
-	obj, err := r.CommitObject(commit)
-	if err != nil {
-		return errors.Wrap(err, "commit failed")
-	}
-
-	_, err = r.CreateTag(version.String(), obj.Hash, &git.CreateTagOptions{
-		Tagger:  signature,
-		Message: version.String(),
-	})
-	if err != nil {
-		return errors.Wrap(err, "create tag failed")
-	}
-
-	return nil
-}
+const changelogFilename = "CHANGELOG.md"
 
 const defaultChangelog = `# Changelog
 
